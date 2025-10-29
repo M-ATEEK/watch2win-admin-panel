cea6a0ed73039cde9995e8b5397ff78900937898
faezeh <abdif701@gmail.com>
2025-10-29 18:16:40 +0200

    added fixes for routing and templates

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,4 @@
 import Dashboard from "views/Dashboard.jsx";
-import UserProfile from "views/UserProfile.jsx";
-import TableList from "views/TableList.jsx";
-import Typography from "views/Typography.jsx";
-import Icons from "views/Icons.jsx";
-import Maps from "views/Maps.jsx";
-import Notifications from "views/Notifications.jsx";
-import Upgrade from "views/Upgrade.jsx";
 import Users from "components/User/Index";
 import Category from "components/Category/Index";
 import Athelete from "components/Athelete/Index";
@@ -31,71 +24,36 @@ const dashboardRoutes = [
 	{
 		path: "/categories",
 		name: "Categories",
-		icon: "pe-7s-user",
+		icon: "pe-7s-back-2",
 		component: Category,
 		layout: "/admin",
 	},
 	{
 		path: "/atheletes",
 		name: "Atheletes",
-		icon: "pe-7s-user",
+		icon: "pe-7s-add-user",
 		component: Athelete,
 		layout: "/admin",
 	},
 	{
 		path: "/difficulty-levels",
 		name: "Difficulty Levels",
-		icon: "pe-7s-user",
+		icon: "pe-7s-edit",
 		component: DifficultyLevel,
 		layout: "/admin",
 	},
 	{
 		path: "/speed-levels",
 		name: "Speed Levels",
-		icon: "pe-7s-user",
+		icon: "pe-7s-edit",
 		component: SpeedLevel,
 		layout: "/admin",
 	},
 	{
 		path: "/drills",
 		name: "Drills",
-		icon: "pe-7s-user",
+		icon: "pe-7s-albums",
 		component: Drill,
-		layout: "/admin",
-	},
-	{
-		path: "/user-form",
-		name: "User Profile",
-		icon: "pe-7s-user",
-		component: UserProfile,
-		layout: "/admin",
-	},
-	{
-		path: "/table",
-		name: "Table List",
-		icon: "pe-7s-note2",
-		component: TableList,
-		layout: "/admin",
-	},
-	{
-		path: "/icons",
-		name: "Icons",
-		icon: "pe-7s-science",
-		component: Icons,
-		layout: "/admin",
-	},
-	{
-		path: "/maps",
-		name: "Maps",
-		icon: "pe-7s-map-marker",
-		component: Maps,
-		layout: "/admin",
-	},
-	{
-		path: "/notifications",
-		name: "Notifications",
-		icon: "pe-7s-bell",
-		component: Notifications,
 		layout: "/admin",
 	},
 ];
