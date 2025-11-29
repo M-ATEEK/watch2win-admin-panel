88a5e85b4d13e446950cdedb3c026331bd0a1a4d
M-ATEEK <muhammadateek.scholars@gmail.com>
2025-11-29 16:30:18 +0200

    added build on git

---
+++ build/precache-manifest.102bf2f8ffebc22eb1a61787e883072a.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "221292431cd859bcdfd1577d07fc0c8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "658cffdae989d07d5533",
+    "url": "/static/css/16.dd3d293f.chunk.css"
+  },
+  {
+    "revision": "0dbba1d1668c6bac2db5",
+    "url": "/static/css/main.2a0293e5.chunk.css"
+  },
+  {
+    "revision": "658cffdae989d07d5533",
+    "url": "/static/js/16.c6534bac.chunk.js"
+  },
+  {
+    "revision": "0dbba1d1668c6bac2db5",
+    "url": "/static/js/main.8bfde493.chunk.js"
+  },
+  {
+    "revision": "92e40e05fe717649d89e",
+    "url": "/static/js/reactPlayerDailyMotion.8107d129.chunk.js"
+  },
+  {
+    "revision": "d9fe4601d16b6fabff4e",
+    "url": "/static/js/reactPlayerFacebook.64b575ed.chunk.js"
+  },
+  {
+    "revision": "dd68723c6a8b6662eb09",
+    "url": "/static/js/reactPlayerFilePlayer.57040147.chunk.js"
+  },
+  {
+    "revision": "d2efcd7024787ffcf67a",
+    "url": "/static/js/reactPlayerKaltura.0789af5e.chunk.js"
+  },
+  {
+    "revision": "c4511006d840300be1f3",
+    "url": "/static/js/reactPlayerMixcloud.797a6ea1.chunk.js"
+  },
+  {
+    "revision": "1b9ec550e43ffa9e4cfd",
+    "url": "/static/js/reactPlayerMux.5a20106b.chunk.js"
+  },
+  {
+    "revision": "56471e6330e53ec0a856",
+    "url": "/static/js/reactPlayerPreview.4b34e43d.chunk.js"
+  },
+  {
+    "revision": "096ffe7585d94c96b740",
+    "url": "/static/js/reactPlayerSoundCloud.f6ae3030.chunk.js"
+  },
+  {
+    "revision": "66f7d7a116b4ee0104c2",
+    "url": "/static/js/reactPlayerStreamable.140c51f3.chunk.js"
+  },
+  {
+    "revision": "41f0dcbae73349c27bec",
+    "url": "/static/js/reactPlayerTwitch.c5fe0f0c.chunk.js"
+  },
+  {
+    "revision": "74b1b0d50ce26b3e10fc",
+    "url": "/static/js/reactPlayerVidyard.2b00e9ba.chunk.js"
+  },
+  {
+    "revision": "921c55f6f29025f1d1a0",
+    "url": "/static/js/reactPlayerVimeo.d09bdb26.chunk.js"
+  },
+  {
+    "revision": "0a1438c466038a09bb03",
+    "url": "/static/js/reactPlayerWistia.81ef967e.chunk.js"
+  },
+  {
+    "revision": "a5407ca140974c8b17ff",
+    "url": "/static/js/reactPlayerYouTube.60eaa056.chunk.js"
+  },
+  {
+    "revision": "4093606f67f161d2d3d2",
+    "url": "/static/js/runtime~main.7757782f.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "c45f7de008ab976a8e817e3c0e5095ca",
+    "url": "/static/media/Pe-icon-7-stroke.c45f7de0.svg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "8cf002e283dd1deeaec8d6dad34d4885",
+    "url": "/static/media/loginBg.8cf002e2.png"
+  },
+  {
+    "revision": "9b864b3680f372ff9ae02649fbc44838",
+    "url": "/static/media/reactlogo.9b864b36.png"
+  },
+  {
+    "revision": "6be21e8a1b7d63048728851c6003e189",
+    "url": "/static/media/sidebar-1.6be21e8a.jpg"
+  },
+  {
+    "revision": "7a4ce7cc040fc1cb8176cde106e9232f",
+    "url": "/static/media/sidebar-2.7a4ce7cc.jpg"
+  },
+  {
+    "revision": "34535b958ad0f0183770a8731b31b4ca",
+    "url": "/static/media/sidebar-3.34535b95.jpg"
+  },
+  {
+    "revision": "7bc259fd5cef15e95376c822c7e23081",
+    "url": "/static/media/sidebar-4.7bc259fd.jpg"
+  }
+]);
